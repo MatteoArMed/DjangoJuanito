e08aa60ac70fa86e98b0c2ef57595beebd5ef6f9
MatteoArMed <mate.araneda@duocuc.cl>
2024-02-08 22:58:49 -0300

    arreglando bugs

--- JuanitoPuntoCom/settings.py
+++ JuanitoPuntoCom/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0j=_nr_1ebkca=h60xgz#6j%+^%2_oo()-rnos6nz0d3-n%0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
-# ALLOWED_HOSTS = ['juanitopuntocom.sa-east-1.elasticbeanstalk.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['juanitopuntocom.sa-east-1.elasticbeanstalk.com']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
