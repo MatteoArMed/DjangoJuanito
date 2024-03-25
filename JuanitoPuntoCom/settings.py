09275f95c1753d74ed8aecc733602c03878cfff1
MatteoArMed <mate.araneda@duocuc.cl>
2024-03-25 14:13:32 -0300

    se actualiza settings para agregar raiz /PuntoCom/ de donde obtiene las imagenes

--- JuanitoPuntoCom/settings.py
+++ JuanitoPuntoCom/settings.py
@@ -132,16 +132,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_URL = os.path.join(BASE_DIR, 'imagenes/')
+# MEDIA_URL = os.path.join(BASE_DIR, 'imagenes/')
+MEDIA_URL = '/PuntoCom/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "PuntoCom/")
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
